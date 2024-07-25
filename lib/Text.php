6785894cf738e83f58cba949f4b8472ad969acbd
Stas <staskazanin@gmail.com>
2024-07-26 00:25:53 +0500

    commit

--- lib/Text.php
+++ lib/Text.php
@@ -3,18 +3,20 @@
     class Text {
 
         public static function spell($n, $s1, $s2, $s3) {
-            if($n >= 11 && $n <= 19) {
+            $n = $n % 100;
+
+            if($n > 9 && $n < 20) {
                 return $s3;
             }
 
             $n = $n % 10;
 
-            if($n == 1) {
-                return $s1;
+            if($n > 1 && $n < 5) {
+                return $s2;
             }
 
-            if($n >= 2 and $n <= 4) {
-                return $s2;
+            if($n == 1) {
+                return $s1;
             }
 
             return $s3;
